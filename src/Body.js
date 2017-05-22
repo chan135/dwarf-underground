29f357b127f5dccb906571a007dda3d9998a9e83
Ivan Kaho Chan <ivan.kaho.chan@gmail.com>
2017-05-22 14:32:31 -0400

    Made 10 total components, only had 5 overall cuz it made more sense

---
+++ src/Body.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Body.css'
+import BodyAuthor from './BodyAuthor'
+import Social from './Social'
+import BodyInfo from './BodyInfo'
+
+class Body extends Component {
+    render() {
+        return(
+            <div className="large-8 medium-12 columns article">
+            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+            <BodyAuthor />
+            <BodyInfo />
+            <Social />
+          </div>
+        )
+    }
+}
+
+export default Body
