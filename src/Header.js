d3075f07bac8f826f687f12c7d7fde0ce36cca9e
Ivan Kaho Chan <ivan.kaho.chan@gmail.com>
2017-05-23 09:12:57 -0400

    Added Nav Links on Header as a map

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,13 @@ import './Header.css'
 import TopHeader from './TopHeader'
 import BotHeader from './BotHeader'
 
+const links = ['home', 'about', 'log in']
+
 class Header extends Component {
   render() {
     return (
       <div className="expanded row header">
-        <TopHeader />
+        <TopHeader links={links}/>
         <BotHeader />
       </div>
     )
--- src/TopHeader.js
+++ src/TopHeader.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import './TopHeader.css'
 
+
 class TopHeader extends Component{
     render() {
+      const links = this.props.links.map(link => <a href="#">{link}</a>)
         return(
             <div className="clearfix">
           <div className="float-left logo">
             <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
           </div>
           <div className="float-right nav-links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Log in</a>
+            {links}
           </div>
         </div>
         )
